7b349301184eb9bfd5380566da5cbee6d92a962e
Vredina-Karedina <tetiana.nikitina.us@gmail.com>
2023-09-18 12:43:01 -0600

    Add sum, extract, multiply and divide functions

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 "Use strict"
+
+const number1 = parseInt(prompt("number1"));
+const number2 = parseInt(prompt("number2"));
+let result;
+
+function sum() {
+    result = number1 + number2;
+    console.log(`sum = ${result}`);
+}
+sum();
+
+function extract() {
+    result = number1 - number2;
+    console.log(`extraction = ${result}`);
+}
+extract();
+
+function multiply() {
+    result = number1 * number2;
+    console.log(`multiplication = ${result}`);
+}
+multiply();
+
+function divide() {
+    result = number1 / number2;
+    console.log(`division = ${result}`);
+}
+divide();
