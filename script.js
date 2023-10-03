bba94292d8ecf394963734920fcfb133897e5d6f
Vredina-Karedina <tetiana.nikitina.us@gmail.com>
2023-10-02 20:04:30 -0600

    Split display to sections to show both numbers

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 "Use strict"
 
-const display = document.querySelector(".display");
-let displayValue = 0192837465;
-display.textContent = displayValue;
-
 let number1;
 let number2;
 let operator;
 let result;
-console.log(operate(number1, number2));
 
-function operate(number1, number2) {
-    const operations = {};
-    operations.add = function() {
-        result = number1 + number2;
-    }
-    operations.subtract = function() {
-        result = number1 - number2;
-    }
-    operations.multiply = function() {
-        result = number1 * number2;
-    }
-    operations.divide = function() {
-        result = number1 / number2;
-    }
+const displayValue = document.querySelector(".value");
+const displayMemory = document.querySelector(".memory");
+let value = 0;
+let memory = 0;
+displayValue.textContent = value;
+displayMemory.textContent = memory;
 
-    operations[`${operator}`]();
-    return result;
-}
+// console.log(operate(number1, number2));
+
+// function operate(number1, number2) {
+//     const operations = {};
+//     operations.add = function() {
+//         result = number1 + number2;
+//     }
+//     operations.subtract = function() {
+//         result = number1 - number2;
+//     }
+//     operations.multiply = function() {
+//         result = number1 * number2;
+//     }
+//     operations.divide = function() {
+//         result = number1 / number2;
+//     }
+
+//     operations[`${operator}`]();
+//     return result;
+// }
