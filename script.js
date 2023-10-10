3ad7821661050a61c0053c9f8f3946a136bef738
Vredina-Karedina <tetiana.nikitina.us@gmail.com>
2023-10-09 20:47:46 -0600

    Not operate on double hyphen when empty numbers

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "Use strict"
 
-let number1;
-let number2;
+let number1 = null;
+let number2 = null;
 let operator;
 let result;
 
@@ -26,7 +26,7 @@ function handleClickButton() {
                     if (displayMemory.textContent) {
                         number2 = value;
                         operate(number1, number2);
-                    };        
+                    };
                     rememberNumber1(this.textContent);
     } else if (this.textContent = "=") {
         number2 = value;
@@ -54,6 +54,7 @@ function rememberNumber1(oper) {
 };
 
 function operate(number1, number2) {
+    if (number1 === null || number2 === null) return;
     operations[operator]();
 
     const resultLength = result.toString().split("").length;
@@ -63,7 +64,7 @@ function operate(number1, number2) {
             value = result;
     } else {
         value = result.toFixed(9 - integerLength);
-    }
+    };
 
     displayValue.textContent = value;
     displayMemory.textContent = null;
@@ -72,13 +73,13 @@ function operate(number1, number2) {
 const operations = {};
 operations["add"] = function() {
     result = number1 + number2;
-}
+};
 operations["subtract"] = function() {
     result = number1 - number2;
-}
+};
 operations["multiply"] = function() {
     result = number1 * number2;
-}
+};
 operations["divide"] = function() {
     result = number1 / number2;
-}
+};
